d014823845bbb40dc6aaad2fa3a1069e29b3e1e7
alegonzalezz <alegonzalez@dc.uba.ar>
2025-02-23 14:05:56 -0300

    change endpoint and resolve it

--- app/customers/create/page.tsx
+++ app/customers/create/page.tsx
@@ -3,6 +3,10 @@
 "use client"; // Este archivo es un componente del cliente
 
 import { useEffect, useState } from 'react';
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { NextResponse } from 'next/server';
+
+const supabase = createClientComponentClient()
 
 export default function Page() {
   const [status, setStatus] = useState<string | null>(null);
@@ -11,33 +15,47 @@ export default function Page() {
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
-    const name = searchParams.get('name');
+    const names = searchParams.get('name');
     const email = searchParams.get('email');
-    const phone = searchParams.get('phone');
+    const phone_number = searchParams.get('phone');
     const dealership_id = searchParams.get('dealership_id');
 
-    if (!name || !email || !phone || !dealership_id) {
+    if (!names || !email || !phone_number || !dealership_id) {
       setError('Missing required parameters');
       return;
     }
 
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `/api/customers/create?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}&dealership_id=${encodeURIComponent(dealership_id)}`,
-          {
-            method: 'GET',
-          }
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-          console.log('Customer created:', data);
-          setStatus('Customer created successfully');
-        } else {
-          console.error('Failed to create customer:', response.statusText);
-          setError('Failed to create customer');
-        }
+        const {  data: clientData, error: clientError } = await supabase
+              .from("client")
+              .select("*")
+              .match({ email: email, phone_number: phone_number });
+        
+              if(clientError || clientData.length > 0){
+                return NextResponse.json({ message: 'Client already exists' }, { status: 200 });
+              }
+        
+            // Inserción de datos en la tabla 'clientes' (incluyendo campos de vehículo)
+            const { data, error } = await supabase
+              .from('client')
+              .insert([
+                {
+                  names,
+                  email,
+                  phone_number,
+                  dealership_id
+                }
+              ]);
+              
+        
+            // Manejar errores de inserción
+            if (error) {
+              console.error('Error inserting data:', error.message);
+              return NextResponse.json({ message: 'Failed to create customer', error
+                }, { status: 500 });
+            }
+            return NextResponse.json({ status: 200 } );
       } catch (error) {
         console.error('Error during fetch:', error);
         setError('Error during fetch');
